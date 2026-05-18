270788d779ee4c40c8fea9a1ddc5059fab4deb7d
oussemakessentini <oussamaksantini@gmail.com>
2026-05-18 16:47:19 -0500

    Initialize Spring Boot backend project

---
+++ secureauth-api/src/test/java/com/jewerly/secureauthapi/SecureauthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewerly.secureauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureauthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
